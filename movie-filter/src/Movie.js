59fece1e86ca32ffad597cedbe004234efab0806
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2022-03-02 18:16:02 -0300

    Create Movie-filter

---
+++ movie-filter/src/Movie.js
@@ -0,0 +1,23 @@
+import { motion } from "framer-motion";
+
+function Movie({ movie }) {
+  return (
+    <motion.div
+      layout
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <h2>{movie.title}</h2>
+      <figure className="container">
+        <img
+          src={`https://image.tmdb.org/t/p/w500` + movie.backdrop_path}
+          alt=""
+        />
+      </figure>
+    </motion.div>
+  );
+}
+
+export default Movie;
